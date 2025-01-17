92a871621cd2842cdddb4e2595da3adcd439389b
bfuemmeler <fuemmelerbk@gmail.com>
2025-01-17 17:48:37 -0600

    Create utils_brenda.py

---
+++ utils_brenda.py
@@ -0,0 +1,8 @@
+'''  
+Module: Stellar Analytics 
+"""
+Module: utils_case
+Purpose: Reusable Module for My Analytics Projects
+Description: This module provides a byline for my analytics projects. 
+When we work hard to write useful code, we want it to be reusable.
+A good byline could be used in every Python analytics project we do.
